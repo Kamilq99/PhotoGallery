8b0e670a403b45f83175d17d1dcbd5baf0e3af28
Kamilq99 <thealti222@gmail.com>
2025-12-14 10:58:23 +0100

    Adding Delete function

--- Backend/s3_config.py
+++ Backend/s3_config.py
@@ -2,10 +2,10 @@ import boto3
 
 s3 = boto3.client(
     "s3",
-    endpoint_url='http://localhost:4566',
-    aws_access_key_id ='YOUR_AWS_ACCESS_KEY_ID',
-    aws_secret_access_key ='YOUR_AWS_SECRET_ACCESS_KEY',
-    region_name ='us-east-1',
+    endpoint_url="http://localhost:4566",
+    aws_access_key_id="test",
+    aws_secret_access_key="test",
+    region_name="us-east-1",
 )
 
-BUCKET_NAME = 'your-bucket-name'
+BUCKET_NAME = "photo-gallery"
